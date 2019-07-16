84dd8c2fc4a74c9bd5efcbcdc28184103f62f2ab
mippo <181391813@qq.com>
2019-07-16 12:51:01 +0800

    替换json库让ios下可以支持泛型

--- Assets/Common/Resource/VersionFileData.cs
+++ Assets/Common/Resource/VersionFileData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,69 @@ namespace Assets.Common.Resource
 {
     public class FileData
     {
+//         private JObject m_obj = null;
         public Int64 size { set; get; }
 
         public string md5 { set; get; }
         public bool local { set; get; }
 
-        public FileData()
-        {
-
-        }
+//         public FileData(JObject obj)
+//         {
+//             m_obj = obj;
+//         }
     }
 
     public class VersionFileData
     {
-        public string version { set; get; }
-        public string url { set; get; }
-        public Dictionary<string, FileData> data { set; get; }
+//         private JObject m_obj = null;
+        public string version
+        {
+            set;
+            //             {
+            //                 m_obj["version"] = value;
+            //             }
+
+            get;
+//             {
+//                 return m_obj["version"].Value<string>();
+//             }
+        }
+        public string url
+        {
+            set;
+            //             {
+            //                 m_obj["url"] = value;
+            //             }
+
+            get;
+//             {
+                //                 return m_obj["url"].Value<string>();
+//             }
+        }
+
+        public Dictionary<string, FileData> data
+        {
+            set;
+//             {
+//                 m_obj["data"] = value;
+//             }
+
+            get;
+//             {
+//                 return m_obj["url"].Value<JObject>();
+//             }
+        }
+// 
+// 
+//         public VersionFileData(JObject obj)
+//         {
+//             m_obj = obj;
+//             var jdata = m_obj["url"].Value<JObject>();
+//             foreach (KeyValuePair<string, JToken> pair in jdata)
+//             {
+//                 data[pair.Key] = new FileData() pair.Value.Value<>
+//             }
+//         }
+
     }
 }
